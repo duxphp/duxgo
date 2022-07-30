88e2977ad47cf5e0be32ee9d8781cf2a96b8090c
Mr.L <admin@duxphp.com>
2022-07-30 15:07:10 +0800

    update

--- config/config.go
+++ config/config.go
@@ -25,6 +25,7 @@ func Init() {
 		name := filename[0 : len(filename)-len(suffix)]
 		core.Config[name] = LoadConfig(name)
 	}
+	fmt.Println("configConfig", core.Config)
 
 	// 解析媒体文件
 	//jsonPath, err := duxgo.StaticFs.Open("public/manifest.json")
